881d09ffc152a08a48bb5dec8f21e601d705ce14
SantinoHamada <hamadasantino@hotmail.com>
2024-08-21 09:59:47 -0300

    se añadio react bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
